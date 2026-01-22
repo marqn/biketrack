746c7d07c71549f4eefda3f89add0a95062909b7
Marqn <marqnpl@gmail.com>
2026-01-22 05:06:41 +0100

    naprawa strony głównej

--- proxy.ts
+++ proxy.ts
@@ -16,5 +16,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/", "/app/:path*", "/onboarding/:path*"],
+  matcher: ["/dashboard/:path*", "/app/:path*", "/onboarding/:path*"],
 };
