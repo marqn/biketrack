f4fc27a3768c68fd93071d6ce18cfe8dcebe3685
Marqn <marqnpl@gmail.com>
2026-03-08 18:37:06 +0100

    fix: problem z nawigacją

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
